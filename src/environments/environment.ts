9f0b7fde3af0649c4dbfc304a434fc6a89ec1e29
Tiago <tiago@autolomous.com>
2023-09-26 20:51:27 +0100

    install firebase@latest @angular/fire@latest

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'tiago-aut-playground',
+    appId: '1:284409907726:web:0996c878d59194a191ca5f',
+    storageBucket: 'tiago-aut-playground.appspot.com',
+    apiKey: 'AIzaSyAzpG1_Zo14b2oDMJdByviviFZmziUAUkU',
+    authDomain: 'tiago-aut-playground.firebaseapp.com',
+    messagingSenderId: '284409907726',
+  },
+};
