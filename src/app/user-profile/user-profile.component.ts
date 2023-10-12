3b13ae4bc999f70b13c0fd7d966f2ca0d64616e1
Tiago <tiago@autolomous.com>
2023-10-12 16:58:31 +0100

    cleaned the code

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { UsersService } from '../users.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
@@ -9,14 +8,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent {
-  user = { firstName: '', lastName: '', email: '' };
-  // constructor(private usersService: UsersService) {}
-  // onSubmit() {
-  //   this.usersService.addUser(this.user); // Call the service's function
-  //   this.user = { name: '', role: '', email: '' };
-  // }
-
   empForm: FormGroup;
+
   constructor(private _fb: FormBuilder, private usersService: UsersService) {
     this.empForm = this._fb.group({
       firstName: '',
@@ -31,6 +24,5 @@ export class UserProfileComponent {
   onFormSubmit() {
     this.usersService.addUser(this.empForm.value); // Send form data to the service
     this.empForm.reset(); // Reset the form
-    console.log(this.empForm.value); // Add this line
   }
 }
